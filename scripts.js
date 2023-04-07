68f3b396ad452fd0459ce2bc1cc045646cb9d2a0
Krimix-dev <130081745+Krimix-dev@users.noreply.github.com>
2023-04-07 22:01:30 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,50 @@
-// Fonction pour récupérer les données des crypto-monnaies
-async function fetchCryptoData() {
-    const apiKey = "37d76517-4ec3-470b-a50f-131cbddd32eb";
-    const apiUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5&convert=USD&CMC_PRO_API_KEY=${apiKey}`;
+const chatButton = document.getElementById('chat-btn');
+const chatContainer = document.getElementById('chat-container');
+const chatForm = document.getElementById('chat-form');
+const chatInput = document.getElementById('chat-input');
+const chatMessages = document.getElementById('chat-messages');
 
-    try {
-        const response = await axios.get(apiUrl);
-        const cryptoList = document.getElementById("crypto-list");
+let conversation = [];
 
-        response.data.data.forEach(crypto => {
-            const listItem = document.createElement("li");
-            listItem.innerHTML = `${crypto.name} (${crypto.symbol}) - ${crypto.quote.USD.price.toFixed
+chatButton.addEventListener('click', () => {
+  chatContainer.style.display = chatContainer.style.display === 'none' ? 'block' : 'none';
+});
+
+chatForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const userMessage = chatInput.value;
+  conversation.push({ role: 'user', content: userMessage });
+  renderConversation();
+  chatInput.value = '';
+
+  const chatGPTMessage = await fetchChatGPT(userMessage);
+  conversation.push({ role: 'assistant', content: chatGPTMessage });
+  renderConversation();
+});
+
+function renderConversation() {
+  chatMessages.innerHTML = conversation
+    .map(message => `<div class="${message.role}">${message.content}</div>`)
+    .join('');
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+async function fetchChatGPT(message) {
+  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUR_API_KEY'
+    },
+    body: JSON.stringify({
+      prompt: `Assistant de cryptomonnaie: ${message}`,
+      max_tokens: 50,
+      n: 1,
+      stop: null,
+      temperature: 0.5,
+    })
+  });
+
+  const data = await response.json();
+  return data.choices[0].text.trim();
+}
